4648ddaeb179fb5fc6fdc3edca96021e33fc37c0
Jesus <jesus.sicairos1@hotmail.com>
2020-03-31 18:50:40 +0000

    added git credentials

--- project/database.sql
+++ project/database.sql
@@ -1,5 +1,7 @@
 CREATE TABLE [users] (
-	[Id] INTEGER  NOT NULL PRIMARY KEY,
+	[Id] INTEGER PRIMARY KEY NOT NULL,
 	[Username] NVARCHAR(100)  NOT NULL,
 	[Password] NVARCHAR(100) NOT NULL
 );
+#add a new table
+
