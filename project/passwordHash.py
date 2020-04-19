57b398ead385fb80e5e869b1d63f94690ff86619
Jesus Sicairos <61787382+JesusRm20@users.noreply.github.com>
2020-04-19 14:12:24 +0100

    Encoding password

--- project/passwordHash.py
+++ project/passwordHash.py
@@ -1,12 +1,13 @@
 import bcrypt
 
 def hashPassword(pwd):
-    password = pwd
+    password = pwd.encode('utf-8')
     hashed = bcrypt.hashpw(password,bcrypt.gensalt())
 
     return hashed
 
 def passwordCheck(pwd, hashedpwd):
+    pwd = pwd..encode('utf-8')
     resp = bcrypt.checkpw(pwd,hashedpwd)
     if resp:
         return True
