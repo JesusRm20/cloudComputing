c59b41c85d458ccdb13db8d3f7dffbc726d28bb9
Jesus Sicairos <61787382+JesusRm20@users.noreply.github.com>
2020-04-19 14:01:40 +0100

    Removed UTF-8

--- project/passwordHash.py
+++ project/passwordHash.py
@@ -2,12 +2,12 @@ import bcrypt
 
 def hashPassword(pwd):
     password = pwd
-    hashed = bcrypt.hashpw(password.encode('utf-8'),bcrypt.gensalt())
+    hashed = bcrypt.hashpw(password,bcrypt.gensalt())
 
     return hashed
 
 def passwordCheck(pwd, hashedpwd):
-    resp = bcrypt.checkpw(pwd.encode('utf-8'),hashedpwd)
+    resp = bcrypt.checkpw(pwd,hashedpwd)
     if resp:
         return True
     else:
