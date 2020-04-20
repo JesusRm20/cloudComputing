1440e106a766c18ec51937f72f13e514357d82c0
Jesus Sicairos <61787382+JesusRm20@users.noreply.github.com>
2020-04-20 02:24:41 +0100

    jdkas

--- project/passwordHash.py
+++ project/passwordHash.py
@@ -4,7 +4,7 @@ def hashPassword(pwd):
     password = pwd.encode('utf-8')
     hashed = bcrypt.hashpw(password,bcrypt.gensalt())
 
-    return hashed
+    return hashed.encode('utf-8')
 
 def passwordCheck(pwd, hashedpwd):
     resp = bcrypt.checkpw(pwd.encode('utf-8'),hashedpwd.encode('utf-8'))
