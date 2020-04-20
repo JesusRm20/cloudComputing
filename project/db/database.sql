717e10b87e1f5dc8044895197e83ba4e9bf05d2e
Jesus Sicairos <61787382+JesusRm20@users.noreply.github.com>
2020-04-20 14:03:53 +0100

    Deleted unnecessary files

--- project/db/database.sql
+++ project/db/database.sql
@@ -1,32 +1,101 @@
-CREATE TABLE [streetLevelCrimes] (
-	[id] INTEGER  NOT NULL PRIMARY KEY,
-	[category_id] NVARCHAR(100),
-	[location_type] NVARCHAR(100),
-	[latitude] NVARCHAR(100),
-	[longitude] NVARCHAR(100) ,
-	[street_id] NVARCHAR(100),
-	[street_name] NVARCHAR(100) ,
-	[context] NVARCHAR(100),
-	[outcome_status] NVARCHAR(100),
-	[persistent_id] NVARCHAR(100),
-	[location_subtype] NVARCHAR(100),
-	[month] NVARCHAR(100)
-);
+-- Table: public."crimeCategories"
 
-CREATE TABLE [outcomesCrimes] (
-	[id] INTEGER  NOT NULL PRIMARY KEY,
-	[persistent_id] NVARCHAR(100),
-	[category] INTEGER,
-	[date_1] NVARCHAR(100),
-	[person_id] NVARCHAR(100)
-);
+-- DROP TABLE public."crimeCategories";
 
-CREATE TABLE [crimeCategories] (
-	[id] INTEGER  NOT NULL PRIMARY KEY,
-	[name] NVARCHAR(100)  NOT NULL
-);
+CREATE TABLE public."crimeCategories"
+(
+    id integer NOT NULL DEFAULT nextval('"crimeCategories_id_seq"'::regclass),
+    name character(200) COLLATE pg_catalog."default",
+    CONSTRAINT "crimeCategories_pkey" PRIMARY KEY (id)
+)
 
-CREATE TABLE [outcomesCount] (
-	[id] INTEGER  NOT NULL PRIMARY KEY,
-	[persistent_id] NVARCHAR(100)  NOT NULL
-);
+TABLESPACE pg_default;
+
+ALTER TABLE public."crimeCategories"
+    OWNER to postgres;
+
+
+-- Table: public."outcomesCount"
+
+-- DROP TABLE public."outcomesCount";
+
+CREATE TABLE public."outcomesCount"
+(
+    id integer NOT NULL DEFAULT nextval('"outcomesCount_id_seq"'::regclass),
+    persistent_id character(200) COLLATE pg_catalog."default",
+    CONSTRAINT "outcomesCount_pkey" PRIMARY KEY (id)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE public."outcomesCount"
+    OWNER to postgres;
+
+
+-- Table: public."outcomesCrimes"
+
+-- DROP TABLE public."outcomesCrimes";
+
+CREATE TABLE public."outcomesCrimes"
+(
+    id integer NOT NULL DEFAULT nextval('"outcomesCrimes_id_seq"'::regclass),
+    persistent_id character(200) COLLATE pg_catalog."default",
+    category character(200) COLLATE pg_catalog."default",
+    date_1 character(200) COLLATE pg_catalog."default",
+    person_id character(200) COLLATE pg_catalog."default",
+    CONSTRAINT "outcomesCrimes_pkey" PRIMARY KEY (id)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE public."outcomesCrimes"
+    OWNER to postgres;
+
+
+-- Table: public."streetLevelCrimes"
+
+-- DROP TABLE public."streetLevelCrimes";
+
+CREATE TABLE public."streetLevelCrimes"
+(
+    id integer NOT NULL,
+    category_id character(200) COLLATE pg_catalog."default",
+    location_type character(200) COLLATE pg_catalog."default",
+    latitude character(200) COLLATE pg_catalog."default",
+    longitude character(200) COLLATE pg_catalog."default",
+    street_id character(200) COLLATE pg_catalog."default",
+    street_name character(200) COLLATE pg_catalog."default",
+    context character(200) COLLATE pg_catalog."default",
+    outcome_status character(200) COLLATE pg_catalog."default",
+    persistent_id character(200) COLLATE pg_catalog."default",
+    location_subtype character(200) COLLATE pg_catalog."default",
+    month character(200) COLLATE pg_catalog."default",
+    CONSTRAINT "streetLevelCrimes_pkey" PRIMARY KEY (id)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE public."streetLevelCrimes"
+    OWNER to postgres;
+
+
+-- Table: public.users
+
+-- DROP TABLE public.users;
+
+CREATE TABLE public.users
+(
+    id integer NOT NULL DEFAULT nextval('users_id_seq'::regclass),
+    name character(200) COLLATE pg_catalog."default",
+    lastname character(200) COLLATE pg_catalog."default",
+    email character(200) COLLATE pg_catalog."default",
+    username character(200) COLLATE pg_catalog."default",
+    password character(200) COLLATE pg_catalog."default",
+    date character(200) COLLATE pg_catalog."default",
+    CONSTRAINT users_pkey PRIMARY KEY (id)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE public.users
+    OWNER to postgres;
